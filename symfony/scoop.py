e4e0d822f79a2ee7aa130638afd10882d04b2647
Mehdi SOMRANI <62431464+Somranii@users.noreply.github.com>
2022-10-29 15:03:21 +0200

    Updating previllage

--- symfony/scoop.py
+++ symfony/scoop.py
@@ -1,6 +1,7 @@
 import subprocess as sp
 
 print('Installing scoop...')
+sp.call("C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe Set-ExecutionPolicy RemoteSigned -Scope CurrentUser", shell=True)
 sp.call("C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe iex (new-object net.webclient).downloadstring('https://get.scoop.sh')", shell=True)
 print("scoop installed...")
 
